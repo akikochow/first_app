da0c63ed756bedac6af26d8b4d3e369702d5730f
akiko chow <akikochow@gmail.com>
2012-08-02 16:21:58 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '817557f6b8fc3f528b8d2175a5ec9bc47147ced92cf2668c482e422a0f6c5d69ae6af9eec44eda2cbb3ee3f7fb25b1ba3c3cb72556cf8fa371c93e14e12f4f8e'
---
+++ sass-rails/lib/sass-rails.rb
@@ -0,0 +1,7 @@
+require "sass-rails/version"
+
+module Sass
+  module Rails
+    # Your code goes here...
+  end
+end
